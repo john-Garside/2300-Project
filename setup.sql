163e675c3f46ea9a40c9a4ed01721eb70349969e
John Garside <skipper@MacBookPro.lan>
2026-04-24 14:08:40 -0500

    finished create table

--- setup.sql
+++ setup.sql
@@ -1,3 +1,7 @@
+-- GROUP PROJECT with Maddox Reed, Zachary Pipes, John Garside
+-- any "user"" in the logical database design diagram
+-- is replaced with the word player because user is a protectec keyword
+
 DROP TABLE IF EXISTS Digital;
 DROP TABLE IF EXISTS Physical;
 DROP TABLE IF EXISTS Belongs_To;
@@ -5,7 +9,7 @@ DROP TABLE IF EXISTS Is_Given;
 DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Tag;
 DROP TABLE IF EXISTS Owns;
-DROP TABLE IF EXISTS Wishlist;
+DROP TABLE IF EXISTS Wishlists;
 DROP TABLE IF EXISTS Achievement;
 DROP TABLE IF EXISTS Game; -- order is to respect dependent tables
 DROP TABLE IF EXISTS Player; -- user is not used because it is a protected keyword
@@ -35,3 +39,61 @@ CREATE TABLE Tag (
   tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL
 );
+
+CREATE TABLE Owns (
+  player_id INTEGER NOT NULL,
+  game_id INTEGER NOT NULL,
+  playtime INTEGER DEFAULT 0,
+  PRIMARY KEY(player_id, game_id)
+  FOREIGN KEY(player_id) REFERENCES Player(player_id),
+  FOREIGN KEY(game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Wishlists(
+  player_id INTEGER NOT NULL,
+  game_id INTEGER NOT NULL,
+  PRIMARY KEY(player_id, game_id),
+  FOREIGN KEY(player_id) REFERENCES Player(player_id),
+  FOREIGN KEY(game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Achievement(
+  player_id INTEGER NOT NULL,
+  game_id INTEGER NOT NULL,
+  name TEXT NOT NULL, -- I have decided to make Achievement text mandatory
+  earned INTEGER DEFAULT 0, -- sqlite doesnt have bool data type :(
+  PRIMARY KEY(player_id, game_id, name), 
+  FOREIGN KEY (player_id) REFERENCES Player(player_id),
+  FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Belongs_To(
+  genre_name TEXT NOT NULL,
+  game_id INTEGER NOT NULL,
+  PRIMARY KEY(genre_name, game_id),
+  FOREIGN KEY (genre_name) REFERENCES Genre(genre_name),
+  FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Is_Given(
+  tag_id INTEGER NOT NULL,
+  game_id INTEGER NOT NULL,
+  PRIMARY KEY(tag_id, game_id),
+  FOREIGN KEY(tag_id) REFERENCES Tag(tag_id),
+  FOREIGN KEY(game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Physical(
+  game_id INTEGER PRIMARY KEY,
+  condition TEXT,
+  shelf_location TEXT,
+  FOREIGN KEY(game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Digital(
+  game_id INTEGER PRIMARY KEY,
+  file_size INTEGER,
+  store_link TEXT,
+  FOREIGN KEY(game_id) REFERENCES Game(game_id)
+);
+
